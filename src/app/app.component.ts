c8a47a447f6d0af282b6abd408e3074c53a69e9d
eunicejoy <eunicejoypostrado@gmail.com>
2021-05-03 17:38:37 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +10,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap/ng-bootstrap';
 export class AppComponent implements OnInit {
   title = 'maintenance-profile';
   enterManagerValidationForm!: FormGroup;
+  resetPasswordForm!: FormGroup;
 
   managerList = [
     {
@@ -24,10 +25,29 @@ export class AppComponent implements OnInit {
     }
   ];
 
+  userList = [
+    {
+      id:1,
+      username:'user001',
+      password:'user111'  
+    },
+    {
+      id:2,
+      username:'user002',
+      password:'user222'
+    }
+  ];
+
+
   constructor(private formBuilder: FormBuilder, private modalService:NgbModal){}
 
   ngOnInit(){
     this.enterManagerValidationForm = this.formBuilder.group({
+      username:[''],
+      password:['']
+    });
+
+    this.resetPasswordForm  = this.formBuilder.group({
       username:[''],
       password:['']
     });
@@ -43,6 +63,24 @@ export class AppComponent implements OnInit {
       username: manager.username,
       password: manager.password
     });
+  }
+
+  openEnterManagerValidationModal(targetModal:any){
+    this.modalService.open(targetModal, {
+      centered: true,
+    });
+  }
+
+  openResetModal(targetModal:any){
+    this.modalService.open(targetModal, {
+      centered:true,
+      backdrop:'static'
+    });
+
+    // this.resetPasswordForm.patchValue({
+    //   username: user.username,
+    //   password: user.password
+    // });
   }
 
   onSubmit(){
